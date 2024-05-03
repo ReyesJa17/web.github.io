e1a3aa10f4a51ec68214f71a78f55e5b818a7231
ReyesJa17 <reyesja17@outlook.com>
2024-05-02 21:56:08 -0600

    Deploy static files to GitHub Pages

--- AletheAIcontent/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'created_at')
-    search_fields = ('title', 'content')
-
-admin.site.register(Post, PostAdmin)
-
